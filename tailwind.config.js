7e30efd7dd86fff68652fd8ea7514e38a251d246
valfaro123 <valfaro6@horizon.csueastbay.edu>
2022-06-29 18:14:14 -0700

    add tailwind css and remove unused styles

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#121619',
+        'selected-text': '#92C9BE',
+        'theme': '#397367',
+        'nav': '#404053',
+        'secondary': '#579E9E',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35',
+      },
+      fontFamily: {
+        'orbitron' : ['"Orbitron"', 'sans-serif']
+      }
+    },
+  },
+  
+}
